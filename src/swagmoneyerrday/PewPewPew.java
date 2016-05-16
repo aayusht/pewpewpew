2ce951641b62171043f9a9805bb1e173c961100d
Aayush <test@Aayushs-MacBook-Pro.local>
2016-05-15 19:11:49 -0700

    even more comments

--- src/swagmoneyerrday/PewPewPew.java
+++ src/swagmoneyerrday/PewPewPew.java
@@ -5,11 +5,17 @@ import javax.swing.JFrame;
 
 public class PewPewPew extends JFrame {
 
+	/**
+	 * constructor that calls the initUI method that actually creates the game
+	 */
     public PewPewPew() {
         
         initUI();
     }
-    
+    /**
+     * a method that creates a new game with a screen of a particular size and centers
+     * the GUI, while specifying how to kill the program (when you exit the application)
+     */
     private void initUI() {
         
         add(new Board());
@@ -21,7 +27,11 @@ public class PewPewPew extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
+    /**
+     * driver that essentially starts the code and creates the window
+     * while updating the GUI based on the events that take place
+     * @param args
+     */
     public static void main(String[] args) {
         
         EventQueue.invokeLater(new Runnable() {
@@ -33,3 +43,4 @@ public class PewPewPew extends JFrame {
         });
     }
 }
+
