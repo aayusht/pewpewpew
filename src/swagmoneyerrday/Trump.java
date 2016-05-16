a30f203f370929f68681cc63ed750686be27e81d
Aayush <test@Aayushs-MacBook-Pro.local>
2016-05-15 20:14:55 -0700

    lots o' comments

--- src/swagmoneyerrday/Trump.java
+++ src/swagmoneyerrday/Trump.java
@@ -1,17 +1,36 @@
 package swagmoneyerrday;
 
+/**
+ * Class to outline a flying object that is the target of the player
+ * @authors Aayush Tyagi, Nikhil Swaminathan, Martin Lee, Paramdeep Atwal, Kevin Lin
+ * @period 5
+ */
 public class Trump extends Sprite {
 
     private double vx;
     private double vy = 0;
     private double a;
+    /**
+     * Constructor to initialize the position and velocity of the object
+     * @param x - x position of the object
+     * @param y - y position of the object
+     * @param vx - velocity of the object
+     * @param a - acceleration of the object
+     */
     public Trump(int x, int y, int vx, double a) {
         super(x, y);
         this.vx = vx;
         this.a = a;
         initAlien();
     }
-    
+    /**
+     * Constructor to initialize object with x and y velocity
+     * @param x - x position of the object
+     * @param y - y position of the object
+     * @param vx - x velocity of the object
+     * @param vy - y velocity of the object
+     * @param a - acceleration of the object
+     */
     public Trump(int x, int y, int vx, int vy, double a) {
         super(x, y);
         this.vx = vx;
@@ -19,14 +38,18 @@ public class Trump extends Sprite {
         this.a = a;
         initAlien();
     }
-
+    /**
+     * Method to load image of object onto the screen
+     */
     private void initAlien() {
-
         loadImage("src/trump.png");
         getImageDimensions();
     }
 
-    //TODO make them come from right or left, maybe add acceleration
+    /**
+     * Method to add movement to the object by changing x and y positions by their velocities
+     * and implement acceleration
+     */
     public void move() {
     	y += (int)vy;
         x += (int)vx;
